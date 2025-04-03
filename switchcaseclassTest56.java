92323cb71ad67ba765b93cc5979f74849ab8af4e
Rutuja901194 <rutujasapkal9011@gmail.com>
2025-04-03 17:08:04 +0530

    switch case
    -

---
+++ switchcaseclassTest56.java
@@ -0,0 +1,23 @@
+ class Test
+{
+   public static void main(String[] args)
+     {
+      int a=10;
+      switch(a)
+ 	{
+	  case 10:System.out.println("Rutuja");
+	  break;
+  
+ 	 case 20:System.out.println("Tanuja");
+	  break;
+
+ 	 case 30:System.out.println("Pooja");
+ 	 break;
+
+	 default:System.out.println("fcjjb");
+ 	 break;
+
+	}
+    }
+
+}

f6d80d3f4fd0bf46bb72e7472c235152431a3473
Rutuja901194 <rutujasapkal9011@gmail.com>
2025-04-05 16:44:46 +0530

    class Test
    -

---
+++ classTest1.java
@@ -0,0 +1,39 @@
+class Test1
+{
+    
+   int NUMBER=10;
+   int Number=11;
+   int number=10; 
+
+}
+class Test2
+{
+    
+  int a=20;
+  int a=30; 
+
+}
+class Test3
+{
+    
+   public static void main(String []args)
+{
+    int String=10;
+    float Exception=10.2f;
+    System.out.println(String);
+    System.out.println(Exception);
+}   
+
+}
+
+class Test4
+{
+    
+   int if=10;
+   int try=11;
+   
+
+}
+
+
+

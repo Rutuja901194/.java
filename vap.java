8b27f0b9299c5373a2c472a5aaef8b60004b698a
Rutuja <rutujasapkal9011@gmail.com>
2025-05-05 23:05:40 +0530

    value added1
    -

---
+++ vap.java
@@ -0,0 +1,18 @@
+public class vap {
+    public static void main(String[] args) {
+        // Declare and initialize 3 character arrays
+        char[] arr1 = {'H', 'e', 'l', 'l', 'o'};
+        char[] arr2 = {'J', 'a', 'v', 'a'};
+        char[] arr3 = {'C', 'h', 'a', 't', 'G', 'P', 'T'};
+
+        // Create strings using these character arrays
+        String str1 = new String(arr1);
+        String str2 = new String(arr2);
+        String str3 = new String(arr3);
+
+        // Print the strings
+        System.out.println("String 1: " + str1);
+        System.out.println("String 2: " + str2);
+        System.out.println("String 3: " + str3);
+    }
+}
